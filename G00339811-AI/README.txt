59b7f63845b29ddaa7737ff7414c57262803c20c
kevin barry <g00339811@gmit.ie>
2019-04-20 14:59:01 +0100

    Fixing up fightable

--- G00339811-AI/README.txt
+++ G00339811-AI/README.txt
@@ -1,17 +1,32 @@
 The  maze  should  be  patrolled  by enemies  (spidersare  already  provided) or  other characters  that are  fullythreaded
+===================================================================================================
 * Fuzzy Logic
 Fuzzy inference system 
-	1.Linguistic variablesand Universe of Discourse
-	2.Fuzzy Setsand Membership Functions	
+	1.Linguistic variable sand Universe of Discourse
+	2.Fuzzy Set sand Membership Functions	
 	3.Defuzzifiers, e.g. COG, COGS, LM, MM, RM.
 	4.Membership   Functions   for   output variables,   e.g. Mamdanior Suegno(singleton values)
-	5.Fuzzy Rules. All fuzzy sets must be covered by at least one fuzzy rule
+	5.Fuzzy Rules. 
+	
+	>How i implemented them with game chars...?
+===================================================================================================
 
 * Neural Netowrk
- The Neural network is traiuned at runtime when the program starts.
- Uses ActivationFunction.Sigmoid, 3, 3, 3);
+	> topology used?
+	> Training and validation data
+ 	> How i implemented them with game chars...?
+ 	
+ rough work: 
+ 	The Neural network is traiuned at runtime when the program starts.
+ 	Uses ActivationFunction.Sigmoid, 3, 3, 3);
+ 	values are normailised
+ 	get next action in fightable uses nn to determine the nexxt action of the spider
+ ===================================================================================================
  
 * Heuristic search
+	> explain implemnation of heursitcs for ai search...
+	> Explain implent of algs.
+	> How i implemented them with game chars...?	
 
 * Extras 
 KeyPress S displays player and game statistics such as health and current weapon. 
@@ -23,3 +38,14 @@ KeyPress S displays player and game statistics such as health and current weapon
 ** Exit node
 	Reaching the exit node (exit sign) with health remaining finishes the game.
 ** Pickups
+
+** Spiders
+Type : 6	Colour : Black	Damage : 80 Searches using A*
+Type : 7	Colour : Blue 	Damage : 60 Searches using A*
+Type : 8	Colour : Brown	Damage : 40
+Type : 9	Colour : Green	Damage : 20
+Type : 10	Colour : Grey	Damage : 10 
+Type : 11	Colour : Orange	Damage : 60
+Type : 12	Colour : Red,	Damage : 40 Searches using A*
+Type : 13	Colour : Yellow	Damage : 20
+
--- G00339811-AI/src/ie/gmit/sw/ai/fuzzy/Fightable.java
+++ G00339811-AI/src/ie/gmit/sw/ai/fuzzy/Fightable.java
@@ -13,26 +13,27 @@ import ie.gmit.sw.ai.traversers.Traversator;
 
 /**
  * Fightable implements runnable to allow enemies to run on separate threads.
+ * This is somewhat a God class for setting up searches, nn decisions and fuzzy
+ * fights.
  * 
  * @author Kevin Barry - Bachelor of Science (Honours) in Software Development
  *
  */
 public class Fightable extends Sprite implements Runnable {
 
-	// variables
+	// Variables that represent a Spider node.
 	private int row;
 	private int col;
-	private int feature;
-
-	private Node node = new Node(row, col, feature);
+	private int spiderType;
+	private Node node = new Node(row, col, spiderType);
 
 	// Lock is used to prevent deadlock in a scenario when multiple threads try to
-	// access a function. 
-	private Object lockThread ;
+	// access a function.
+	private Object lockThread;
 
 	// KeepThreadAlive is used to control the live/die state of a thread.
 	private Boolean keepThreadAlive;
-	
+
 	private Node[][] maze;
 
 	private Random random = new Random();
@@ -46,52 +47,73 @@ public class Fightable extends Sprite implements Runnable {
 
 	// The damage a spider can inflict
 	private int fighterDamage;
-	
-	// 
+
+	//
 	// searches
 
 	/**
+	 * Constructor Fightable initilise a spider as a fightable objec. A fightable
+	 * runs on its own seperate thread and allows selected spiders to perform
+	 * actions such as, search for player, follow player, use neural network to
+	 * decide to fight or run and calculate fight damage using fuzzy logic.
 	 * 
-	 * @param row
-	 * @param col
-	 * @param feature
-	 * @param lock    the lock for ythe executor
-	 * @param maze    the game maze
-	 * @param p       the player
-	 * @param f       the neural netwprl
+	 * @param      row, the row that the fightable is located in the maze.
+	 * @param      col, the row that the fightable is located in the maze.
+	 * @param      spiderType, the type of spider.
+	 * @param lock the lock for the executor
+	 * @param maze the game maze
+	 * @param p    the player
+	 * @param f    the neural netwprl
 	 */
-	public Fightable(int row, int col, int feature, Node[][] maze, player p,int damage,Object lockThread) {
-		// TODO Auto-generated constructor stub
+
+	/**
+	 * Constructor Fightable initilise a spider as a fightable objec. A fightable
+	 * runs on its own seperate thread and allows selected spiders to perform
+	 * actions such as, search for player, follow player, use neural network to
+	 * decide to fight or run and calculate fight damage using fuzzy logic.
+	 * 
+	 * @param row, the row that the fightable is located in the maze.
+	 * @param col, the row that the fightable is located in the maze.
+	 * @param spiderType, the type of spider.
+	 * @param maze, 2-Dim node array of the maze.
+	 * @param p, the player object.
+	 * @param damage, the damage that the spider can give.
+	 * @param lockThread, Object to lock thread for synchronisation to avoid
+	 *        deadlock.
+	 */
+	public Fightable(int row, int col, int spiderType, Node[][] maze, player p, int damage, Object lockThread) {
+		// Set the spider.
 		this.row = row;
 		this.col = col;
-		this.feature = feature;
-		// player
-		this.player = p;
+		this.spiderType = spiderType;
 		this.fighterDamage = damage;
+
+		// Set the player.
+		this.player = p;
 
 		node.setCol(col);
 		node.setRow(row);
-		node.setType(feature);
+		node.setType(spiderType);
 
+		// Set the maze.
 		this.maze = maze;
 
-		//this.player.setPlayerHealth(100);
+		// Set thread variables.
 		this.keepThreadAlive = true;
-		this.lockThread= lockThread;
-		//this.nnfight = f;
+		this.lockThread = lockThread;
 
-		// ONLY GIVING 2 spiders follow
-		if (feature == 6) {// fuzzy spider
-			// assign a search
+		/*
+		 * Allows spiders { 6: Black, 11: Orange, 12: Red} too search. Spiders search
+		 * for player node using the A* search algorithim.
+		 */
+		if (spiderType == 6) {
 			System.out.println("Black spider searching for player using AStarTraversator");
-
 			traverse = new AStarTraversator(this.player);
-		} else if (feature == 11) {// nn spider
+		} else if (spiderType == 11) {// nn spider
 			System.out.println("Orange spider searching for player using AStarTraversator");
 			traverse = new AStarTraversator(this.player);
-		} else if (feature == 12) {// nn spider
+		} else if (spiderType == 12) {// nn spider
 			System.out.println("Red spider searching for player using AStarTraversator");
-
 			traverse = new AStarTraversator(this.player);
 		}
 
@@ -99,14 +121,16 @@ public class Fightable extends Sprite implements Runnable {
 
 	@Override
 	public void run() {
+		// Keep thread running while true.
 		while (this.keepThreadAlive) {
 			try {
-				Thread.sleep(1000 * feature / 2);
+				Thread.sleep(1000 * spiderType / 4);
 
-				// 6 = Black Spider - 7 = Blue spider -  8 = Brown Spider - 9 = Greewn spider..
-				if (feature >= 6 && feature <= 9) {
+				// 6 = Black Spider - 7 = Blue spider - 8 = Brown Spider - 9 = Greewn spider..
+				if (spiderType >= 6 && spiderType <= 9) {
+
 					// 6 is black searching spider
-					if (feature == 6) {
+					if (spiderType == 6) {
 						traverse(node.getRow(), node.getCol(), traverse);
 					}
 
@@ -115,7 +139,7 @@ public class Fightable extends Sprite implements Runnable {
 						System.out.println("Spider has caught the player !");
 						// start a fight.
 						fightFuzzy(fighterDamage);
-						
+
 					} else if (canMove && node.getHeuristic(this.player) < 10) {
 						System.out.println("Spider(fuzzy) is withen 10 squares and is now following player");
 						// follow player.
@@ -126,9 +150,9 @@ public class Fightable extends Sprite implements Runnable {
 					}
 
 				} else {
-					
+
 					// 11 = Orange Spider - 12 = Red spider.
-					if (feature == 11 || feature == 12) {
+					if (spiderType == 11 || spiderType == 12) {
 						traverse(node.getRow(), node.getCol(), traverse);
 					}
 					if (node.getHeuristic(this.player) == 1) {
@@ -136,8 +160,9 @@ public class Fightable extends Sprite implements Runnable {
 						// start a fight
 						fightFuzzy(fighterDamage);
 					} else if (canMove && node.getHeuristic(player) < 5) {
-						System.out.println("Spider(Neural Net) is withen 10 squares and is now following player canmove");
-						fightNn(this.player.getPlayerHealth(),fighterDamage, 1);
+						System.out
+								.println("Spider(Neural Net) is withen 10 squares and is now following player canmove");
+						getNextAction(this.player.getPlayerHealth(), fighterDamage, 1);
 
 					} else {
 						// Move randomly
@@ -269,17 +294,18 @@ public class Fightable extends Sprite implements Runnable {
 	 */
 	public void fightFuzzy(int spiderPower) {
 		FuzzyFight fuzzyFight = new FuzzyFight();
-		
+
 		// Carry out a fuzzy attack.
 		double damageToGive = fuzzyFight.getPlayerDamage(player.getWeapon().getDamage(), spiderPower);
-		
+
 		// Remove attack damage from players health.
 		player.setPlayerHealth(player.getPlayerHealth() - damageToGive);
 
-		//System.out.println("Fuzzy fight damage" + Math.round(damageToGive) + "player health now :" + Math.round(player.getPlayerHealth()));
-		Utils.displayWarningInfo("You just lost "+ Math.round(damageToGive) +"health in that battle !", "Youve been attacked");
-		
-		this.keepThreadAlive=false;
+		// System.out.println("Fuzzy fight damage" + Math.round(damageToGive) + "player
+		// health now :" + Math.round(player.getPlayerHealth()));
+		Utils.displayWarningInfo("You just lost " + Math.round(damageToGive) + "health in that battle !",
+				"Youve been attacked");
+		this.keepThreadAlive = false;
 		// shut her down
 		if (player.getPlayerHealth() < 0) {
 			Utils.displayWarningInfo("Game Over your Health reached 0 !", "GameOver");
@@ -287,9 +313,22 @@ public class Fightable extends Sprite implements Runnable {
 		}
 	}
 
+	/**
+	 * Retrieves the next move using the specified traversator algorithim.
+	 * 
+	 * @param row, current row of node.
+	 * @param col, current location of node.
+	 * @param t, the traversator algorithm to use.
+	 */
 	public void traverse(int row, int col, Traversator t) {
+
+		// Traverse the maze.
 		t.traverse(maze, maze[row][col]);
+
+		// Get the next position.
 		nextPosition = t.getNextNode();
+
+		// Check if position is free to move to.
 		if (nextPosition != null) {
 			canMove = true;
 		} else {
@@ -297,10 +336,21 @@ public class Fightable extends Sprite implements Runnable {
 		}
 	}
 
-	public void fightNn(double health, double angerLevel, double weapon) {
-		System.out.println("In fighnnt meythod in fightable");
+	/**
+	 * getNextActio uses previously trained neural network to determine the action
+	 * of the spider.
+	 * 
+	 * @param health, The players health.
+	 * @param spiderPower, The power of the spider.
+	 * @param weapon the players current weapon.
+	 */
+	public void getNextAction(double health, double spiderPower, double weapon) {
+		
+		System.out.println("[INFO : Neural Network] A spider is calculating its next action."+weapon);
 
-// NORMAILIZE VALUES++=========================
+		// Must Normalise the values before feeding them into the neural network.
+
+		// Normalise health.z
 		if (health < 30) {
 			health = 0;
 		} else if (health > 30 && health < 60) {
@@ -309,44 +359,49 @@ public class Fightable extends Sprite implements Runnable {
 			health = 2;
 		}
 
-		if (angerLevel < 30) {
-			angerLevel = 0;
-		} else if (angerLevel > 30 && angerLevel < 60) {
-			angerLevel = 1;
-		} else if (angerLevel > 60) {
-			angerLevel = 2;
+		// Normalise spiders power.
+		if (spiderPower < 30) {
+			spiderPower = 0;
+		} else if (spiderPower > 30 && spiderPower < 60) {
+			spiderPower = 1;
+		} else if (spiderPower > 60) {
+			spiderPower = 2;
 		}
 
+		// Generate next outcome from nerual net.
+		// 1: panic (move randomly)
+		// 2: attack (start following player)
+		// 3: hide (move away from player)
 		try {
 
-			outcome = nnfight.action(health, weapon, angerLevel);
+			outcome = nnfight.action(health, weapon, spiderPower);
 			// checkToRetrain =false;
 			if (outcome == 1) {
-				System.out.println("panic");
+				System.out.println("[INFO : Neural Network] The spiders next astion will be panic.");
 				Utils.displayWarningInfo("The closest spider has gone into a panic state !",
 						"Neural Network Activated");
 				move();
 			} else if (outcome == 2) {
-				System.out.println("attack");
+				System.out.println("[INFO : Neural Network] The spiders next astion will be attack.");
 				Utils.displayWarningInfo("The closest spider has gone into a attack state and is now following !",
 						"Neural Network Activated");
-
 				followPlayer();
 			} else if (outcome == 3) {
+				System.out.println("[INFO : Neural Network] The spiders next astion will be hide.");
 				Utils.displayWarningInfo("The closest spider has gone into a hiding state !",
 						"Neural Network Activated");
 
-				// bang in some run away stuff
+				// Can adapt to make spider run away.
 				move();
-				System.out.println("hide");
-
+			} else {
+				System.out.println("[INFO : Neural Network] The spiders next astion will be move.");
+				move();
 			}
-
-			else
-				move();
 		} catch (Exception e) {
+			// If an exception occurs print it.
+			e.printStackTrace();
 		}
-		
+
 	}
 
 }
